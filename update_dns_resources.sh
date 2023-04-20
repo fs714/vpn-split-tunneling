69a74438bf0ecb48af1e1ca0d9ea18defb1dc641
shuai.fang <fs714@163.com>
2023-04-20 03:39:35 +0000

    Add gfw_additional.conf and update resouces on 20230420

--- update_dns_resources.sh
+++ update_dns_resources.sh
@@ -12,7 +12,7 @@ rm china_list1.txt china_list2.txt china_list3.txt
 
 curl -s https://raw.githubusercontent.com/Loukky/gfwlist-by-loukky/master/gfwlist.txt | base64 -d | sort -u | sed '/^$\|@@/d'| sed 's#!.\+##; s#|##g; s#@##g; s#http:\/\/##; s#https:\/\/##;' | sed '/\*/d; /apple\.com/d; /sina\.cn/d; /sina\.com\.cn/d; /baidu\.com/d; /qq\.com/d' | sed '/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$/d' | grep '^[0-9a-zA-Z\.-]\+$' | grep '\.' | sed 's#^\.\+##' | sort -u > gfwlist.txt
 curl -s https://raw.githubusercontent.com/hq450/fancyss/3.0/rules/gfwlist.conf | sed 's/ipset=\/\.//g; s/\/gfwlist//g; /^server/d' > koolshare.txt
-cat gfwlist.txt koolshare.txt | sort -u > gfw_list.txt
+cat gfwlist.txt koolshare.txt gfw_additional.conf | sort -u > gfw_list.txt
 sed -i '/m-team/d' gfw_list.txt
 sed -i '/windowsupdate/d' gfw_list.txt
 sed -i '/v2ex/d' gfw_list.txt
