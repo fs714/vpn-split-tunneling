36af86c0549d9d2c339383003df0b458a021fc0e
fs714 <fs714@163.com>
2023-04-24 12:33:29 +0800

    Always set WLAN metric to 2 before config route

--- clean_all.bat
+++ clean_all.bat
@@ -5,10 +5,12 @@ ver|find "5.">nul&&goto :st
 mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :st","","runas",1)(window.close)&goto :eof
 
 :st
-netsh interface ipv4 set interface interface=WLAN metric=auto
+netsh interface ipv4 set interface interface=WLAN metric=2
 netsh interface ipv4 set dnsservers name=WLAN source=dhcp
 wmic process where (commandline like "%%overture-windows-amd64.exe%%" and not name="wmic.exe") delete
 
 copy "%~0" "%windir%\system32\"
 cd /d %~dp0
 rundll32.exe cmroute.dll,SetRoutes /STATIC_FILE_NAME del.txt /DONT_REQUIRE_URL /IPHLPAPI_ACCESS_DENIED_OK
+
+netsh interface ipv4 set interface interface=WLAN metric=auto
--- clean_route.bat
+++ clean_route.bat
@@ -3,6 +3,10 @@
 ver|find "5.">nul&&goto :st
 mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :st","","runas",1)(window.close)&goto :eof
 :st
+netsh interface ipv4 set interface interface=WLAN metric=2
+
 copy "%~0" "%windir%\system32\"
 cd /d %~dp0
 rundll32.exe cmroute.dll,SetRoutes /STATIC_FILE_NAME del.txt /DONT_REQUIRE_URL /IPHLPAPI_ACCESS_DENIED_OK
+
+netsh interface ipv4 set interface interface=WLAN metric=auto
